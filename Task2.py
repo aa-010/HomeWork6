a0db8aa411a806929bb4c8fcf0c07f6d2b7709e6
aa-010 <290949@gmail.com>
2020-01-14 11:28:43 +0200

    Create Task2.py

---
+++ Task2.py
@@ -0,0 +1,27 @@
+
+str = "9.9"
+
+list_version = []
+
+
+for i in range(0, len(str)):
+    if(str[i] != '.'):
+        list_version.append(int(str[i]))
+
+print(list_version)
+
+for i in range(len(list_version)-1 , -1, -1):
+    if (list_version[i] + 1 != 10) and (i !=0):
+        list_version[i] += 1
+        break
+    elif (len(list_version) == 1):
+        list_version[0] += 1
+    else:
+        list_version[i] = 0
+        list_version[i-1] += 1
+        break
+
+
+print(list_version)
+#list_version = '.'.join(str(list_version))
+#print('.'.join(map(str, list_version)))
